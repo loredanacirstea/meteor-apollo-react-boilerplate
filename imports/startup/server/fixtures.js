c047950a58d57843d1e294be60460aac8fe7035e
Loredana Cirstea <loredana.cirstea@gmail.com>
2016-07-17 17:21:13 +0300

    added some initial data & temporary code for updating the DB after a field rename

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1,1 +1,49 @@
 // fill the DB with example data on startup
+import Lists from '../../api/lists/lists';
+import Tasks from '../../api/tasks/tasks';
+
+let listdata = [
+  {
+    "_id" : "578ac00dc77bd3835daa0589",
+    "title" : "To Dos",
+    "description" : "Things that I have to do",
+    "ordering" : 1,
+    "noOfTasks" : 4,
+    "creator" : "Lore",
+    "sublist" : {
+        "field1" : "sublist field1",
+        "field2" : "sublist field2"
+    },
+    "locked" : false
+  }
+];
+
+let taskdata = [
+  {
+    "_id" : "578ac0b7c77bd3835daa058a",
+    "list" : "578ac00dc77bd3835daa0589",
+    "title" : "Do a video",
+    "description" : "Do a video demo of schema-graphql-bridge",
+    "ordering" : 1,
+    "creator" : "Lore",
+    "status" : 0
+  },
+  {
+    "_id" : "578ac0e8c77bd3835daa058b",
+    "list" : "578ac00dc77bd3835daa0589",
+    "title" : "Write a README.md file",
+    "description" : "Write a README.md file for schema-graphql-bridge",
+    "ordering" : 2,
+    "creator" : "Lore",
+    "status" : 0
+  }
+];
+
+if(!Lists.findOne()) {
+  listdata.forEach(d => {
+    Lists.insert(d);
+  });
+  taskdata.forEach(d => {
+    Tasks.insert(d);
+  });
+}
--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -15,3 +15,51 @@ import './fixtures.js';
 // as an API to the client.
 import Lists from '../../api/lists/lists';
 import Tasks from '../../api/tasks/tasks';
+
+// This code updates the database when a field name is changed in the Simple Schema
+// Constraints: only 1 field changed, no additions, no deletions
+const getModifier = (keys1, keys2) => {
+  let idi = keys1.indexOf('_id');
+  if(idi != -1)
+    keys1.splice(idi, 1);
+
+  if(keys1.length != keys2.length)
+    return;
+
+  let changed1 = [], changed2 = [];
+  for(k1 in keys1)
+    if(keys2.indexOf(keys1[k1]) == -1)
+      changed1.push(keys1[k1]);
+  for(k2 in keys2)
+    if(keys1.indexOf(keys2[k2]) == -1)
+      changed2.push(keys2[k2]);
+  let modifier = {};
+  for(i in changed1)
+    modifier[changed1[i]] = changed2[i];
+
+  if(!Object.keys(modifier).length)
+    return;
+  return {$rename: modifier};
+}
+
+const updateAll = (Collection, modifier) => {
+  Collection.update({}, modifier, {multi: true, validate: false, filter: false});
+}
+
+Meteor.startup(() => {
+  let l = Lists.findOne();
+  let t = Tasks.findOne();
+
+  let updl = getModifier(Object.keys(l), Lists.schema._firstLevelSchemaKeys);
+  let updt = getModifier(Object.keys(t), Tasks.schema._firstLevelSchemaKeys);
+
+  console.log('updl: ')
+  console.log(updl)
+  console.log(updt)
+
+  if(updl)
+    updateAll(Lists, updl);
+  if(updt)
+    updateAll(Tasks, updt);
+
+});
