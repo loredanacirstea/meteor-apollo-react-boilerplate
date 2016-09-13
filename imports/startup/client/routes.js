44e091495912c8c96834751473b56113e8f8ad06
Loredana Cirstea <loredana.cirstea@gmail.com>
2016-09-14 00:17:06 +0300

    added react-router

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,21 +1,38 @@
-/*
-  set up all routes in the app
-  // Import to load these templates
-  import '../../ui/layouts/app-body.js';
-  import AppContainer from '../../ui/containers/AppContainer.js';
-  import ListContainer from '../../ui/containers/ListContainer.js';
-
-  FlowRouter.route('/lists/:_id', {
-    name: 'Lists.show',
-    action() {
-      mount(AppContainer, {
-        main: <ListContainer/>,
-      });
-    },
-  });
-
-*/
-
 import React from 'react';
-import { FlowRouter } from 'meteor/kadira:flow-router';
-import { mount } from 'react-mounter';
+import { render } from 'react-dom'
+import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router'
+
+import Home from '../../ui/components/home';
+import Lists from '../../ui/components/lists';
+
+$(document.body).append('<div id="AppBody"></div>');
+
+const App = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>App</h1>
+        <ul>
+          {/*<li><Link to='/about'>About</Link></li>*/}
+        </ul>
+
+        {/*
+          next we replace `<Child>` with `this.props.children`
+          the router will figure out the children for us
+        */}
+        {this.props.children}
+      </div>
+    )
+  }
+})
+
+// Finally, we render a <Router> with some <Route>s.
+// It does all the fancy routing stuff for us.
+render((
+  <Router history={hashHistory}>
+    <Route path='/' component={App}>
+      <IndexRoute component={Home} />
+      <Route path='lists' component={Lists} />
+    </Route>
+  </Router>
+), document.getElementById('AppBody'))
---
+++ imports/ui/components/lists.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Lists = React.createClass({
+	render() {
+		return (
+			<div></div>
+		);
+	}
+});
+
+export default Lists;
