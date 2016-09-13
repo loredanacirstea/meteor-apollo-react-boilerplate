8ba5566e5fe677455bbe0a8e8bc8b0b88b5033e5
Loredana Cirstea <loredana.cirstea@gmail.com>
2016-09-14 00:30:05 +0300

    Started trying out a MongoDB connector

--- imports/api/connectors.js
+++ imports/api/connectors.js
@@ -1,3 +1,48 @@
+import { MongoConnector } from 'apollo-connector-mongodb');
+
+const mongoURL = 'mongodb://localhost:3001/test';
+
+const conn = new MongoConnector(mongoURL, () => {});
+
+/*const conn = new MongoConnector(mongoURL, () => {
+
+  //init express and apollo
+  const context = initContext(conn);
+  const config = {
+    schema,
+    pretty : true,
+    context
+  };
+
+  // launches a new express instance
+  startExpress(config);
+});*/
+
+class Model {
+  constructor({connector}) {
+    this.connector = connector;
+  }
+  findOne(id) {
+    return this.connector.findOne(id);
+  }
+  find() {
+    return this.connector.find();
+  }
+}
+
+
+
+export const connectors = {
+  couchDB: new CouchDBConnector(couchDB)
+}
+
+export const models = {
+  List: new Model({ db: 'couchDB' }),
+  Task: new Model({ db: 'couchDB' }),
+}
+
+
+
 /*
 import { CouchDBConnector } from 'meteor/kuip:apollo-connector-couchdb';
 
